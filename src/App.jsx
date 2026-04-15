c5561049966cc656d9ecf5e8900bfcb5aeed286e
Zingjul <nandomshepherd@gmail.com>
2026-04-15 17:23:24 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <FeatureSplit
           title="Master of Silence."
           description="Active Noise Cancellation continuously adapts to the geometry of your ear and the fit of the ear tips to block out the world."
-          imageUrl="/luxury_black_earbuds.png"
+          imageUrl="./luxury_black_earbuds.png"
           reversed={false}
         />
       </section>
@@ -27,7 +27,7 @@ function App() {
         <FeatureSplit
           title="The Sound of Science."
           description="Custom-built high‑excursion, low-distortion driver delivers powerful bass. A super-efficient high-dynamic-range amplifier produces pure, incredibly clear sound."
-          imageUrl="/inview.png"
+          imageUrl="./inview.png"
           reversed={true}
         />
       </section>
@@ -36,7 +36,7 @@ function App() {
         <FeatureSplit
           title="For your ears only."
           description="Personalized audio profiles dynamically map sound to your unique hearing signature. Immerse yourself in a sonic landscape tailored exclusively for you."
-          imageUrl="/pairview.png"
+          imageUrl="./pairview.png"
           reversed={false}
         />
       </section>
